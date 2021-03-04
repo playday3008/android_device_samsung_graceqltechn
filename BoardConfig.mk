0c355d42ee2575f564a721d7e5bdd5d5d83e56a2
playday3008 <titanv3585@gmail.com>
2021-03-04 05:52:29 +0100

    Code refactoring

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,20 +1,22 @@
-DEVICE_TREE := device/samsung/graceqltechn
+#
+# Copyright (C) 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Bootloader
-TARGET_NO_BOOTLOADER := true
-TARGET_BOOTLOADER_BOARD_NAME := msm8996
+LOCAL_PATH := device/samsung/graceqltechn
 
-# Platform
-TARGET_BOARD_PLATFORM := msm8996
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno530
-TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
-
-# Flags
-#TARGET_GLOBAL_CFLAGS +=
-#TARGET_GLOBAL_CPPFLAGS +=
-#COMMON_GLOBAL_CFLAGS +=
-
-# Architecture
+## Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
@@ -28,6 +30,22 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+# Platform
+TARGET_BOARD_PLATFORM := msm8996
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno530
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
+TARGET_IS_64_BIT := true
+TARGET_BOARD_SUFFIX := _64
+TARGET_USES_64_BIT_BINDER := true
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := msm8996
+
+# Crypto
+TW_INCLUDE_CRYPTO := true
+TARGET_HW_DISK_ENCRYPTION := true
+
 # Kernel
 #TARGET_KERNEL_SOURCE := kernel/samsung/msm8996
 TARGET_KERNEL_ARCH := arm64
@@ -35,16 +53,16 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CONFIG := twrp_defconfig
 TARGET_KERNEL_DEVICE_DEFCONFIG := device_graceqlte_chnzc
 
-TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/Image.gz
-TARGET_PREBUILT_DTB := $(DEVICE_TREE)/dtb.img
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/Image.gz
+TARGET_PREBUILT_DTB := $(LOCAL_PATH)/dtb.img
 
 # Boot image
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.bootdevice=624000.ufshc androidboot.selinux=permissive user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=24M@0-0xffffffff rcupdate.rcu_expedited=1
 BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.bootdevice=624000.ufshc androidboot.selinux=permissive user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=24M@0-0xffffffff rcupdate.rcu_expedited=1
 BOARD_KERNEL_PAGESIZE := 4096
 # 000RU = recovery kernel, 000KU = system kernel
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02200000 --tags_offset 0x02000000 --board RILPE03A000RU
-BOARD_CUSTOM_BOOTIMG_MK :=  $(DEVICE_TREE)/bootimg.mk
+BOARD_CUSTOM_BOOTIMG_MK :=  $(LOCAL_PATH)/bootimg.mk
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x004C00000
@@ -52,12 +70,15 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x004C00000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 0x14E600000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x64BFFC000 # 0x64C000000 - 16384 (footer)
 BOARD_CACHEIMAGE_PARTITION_SIZE    := 0x012C00000
+BOARD_PERSISTIMAGE_PARTITION_SIZE  := 0x002000000
 BOARD_FLASH_BLOCK_SIZE := 0x40000
 
-# File systems
+## Recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
+
 
 # TWRP specific build flags
 TW_THEME := portrait_hdpi
@@ -71,17 +92,9 @@ TW_DEFAULT_BRIGHTNESS := 162
 TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
 TW_INCLUDE_NTFS_3G := true
-# exFAT drivers included in the kernel
 TW_NO_EXFAT_FUSE := true
-# No love for the wicked (device ships with M)
 TW_EXCLUDE_SUPERSU := true
-
-# Encryption support
-TW_INCLUDE_CRYPTO := true
-# Disable Qualcomm and Samsung encryption as we are missing msm8996 keystore support
-#TW_INCLUDE_CRYPTO_SAMSUNG := true
-#TARGET_HW_DISK_ENCRYPTION := true
-#TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
 
 # Asian region languages
 TW_EXTRA_LANGUAGES := true
--- omni_graceqltechn.mk
+++ omni_graceqltechn.mk
@@ -1,11 +1,30 @@
-# Release name
-PRODUCT_RELEASE_NAME := graceqltechn
+#
+# Copyright (C) 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
-# Inherit from our custom product configuration
+# Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit Telephony packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit language packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 PRODUCT_PACKAGES += \
 	charger_res_images \
@@ -13,7 +32,10 @@ PRODUCT_PACKAGES += \
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := graceqltechn
-PRODUCT_MODEL := SM-N9300
 PRODUCT_NAME := omni_graceqltechn
-PRODUCT_BRAND := samsung
-PRODUCT_MANUFACTURER := samsung
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := SM-N9300
+PRODUCT_MANUFACTURER := Samsung
+
+# Release name
+PRODUCT_RELEASE_NAME := graceqltechn
